7ada00ad78d400f70ecef687aa4fa17c73c064fc
mohamed amin barri <barrimohamed01@gmail.com>
2025-01-29 13:44:17 +0100

    add some db

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    echo 'hello world';
+    return view('main');
 });
+
+Route::get('/tasks', [TaskController::class, 'store'])->name('task');
+Route::get('/singup', [UserController::class, 'store'])->name('singup');
+Route::post('/singup/create', [UserController::class, 'create'])->name('create');
+
